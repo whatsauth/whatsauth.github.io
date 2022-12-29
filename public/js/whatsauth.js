7a04817dd12158ab693c2e6bf2cadaac237a99e9
Rolly Maulana Awangga <rolly@awang.ga>
2022-12-29 23:05:28 +0700

    add checkcookie

--- public/js/whatsauth.js
+++ public/js/whatsauth.js
@@ -43,7 +43,7 @@ function showQR(text){
 
 function changePhoneNumber(){
   deleteCookie("phonenumber");
-  console.log("phone number dihapus");
+  checkCookie();
 }
 
 function setCookieWithExpireDay(cname, cvalue, exdays) {
