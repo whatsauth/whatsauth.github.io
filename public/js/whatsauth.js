f8dca441a00de4375ff7c0d51d0ca31e830a51d3
Rolly Maulana Awangga <rolly@awang.ga>
2022-12-29 23:13:24 +0700

    change phone number

--- public/js/whatsauth.js
+++ public/js/whatsauth.js
@@ -1,7 +1,7 @@
 const url="/public/qr/qr.json";
 
 let logoutbutton = `   <button onclick="changePhoneNumber()" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-0 px-1 border border-blue-500 hover:border-transparent rounded">
-Logout
+Change
 </button>`;
 
 
