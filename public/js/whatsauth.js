55bd87b26af751e35373591ed790a7df70766854
Rolly Maulana Awangga <rolly@awang.ga>
2022-12-29 21:55:56 +0700

    add button logout

--- public/js/whatsauth.js
+++ public/js/whatsauth.js
@@ -1,8 +1,7 @@
 const url="/public/qr/qr.json";
 
-let logoutbutton = `<button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-<svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-<span>Logout</span>
+let logoutbutton = `   <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+Logout
 </button>`;
 
 
