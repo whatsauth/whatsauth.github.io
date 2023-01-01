6493a50f3c8be6eb2b2af9ae79928dd94f2e2737
Rolly Maulana Awangga <rolly@awang.ga>
2023-01-01 09:53:06 +0700

    uuid

--- public/js/whatsauth.js
+++ public/js/whatsauth.js
@@ -9,8 +9,9 @@ function main() {
   let uuid = getCookie("uuid");
   if (uuid != "") {
     setCookieWithExpireSecond("uuid",crypto.randomUUID(),10);
-    getCookie("uuid")
+    uuid = getCookie("uuid")
   }
+  console.log(uuid)
   qrController(uuid);
 }
 
